5e2f8957f2adf5b6fa06457eb113cd0148247a0b
Christopher Webster <christopher.webster@one.verizon.com>
2015-05-26 21:06:20 -0700

    initial benchmark

---
+++ build.sbt
@@ -0,0 +1,6 @@
+enablePlugins(JmhPlugin)
+
+
+libraryDependencies ++= Seq(
+  "net.liftweb" % "lift-json_2.11" % "3.0-M2"
+)
---
+++ src/main/scala/LiftJsonBenchmark.scala
@@ -0,0 +1,19 @@
+package liftbenchmark
+
+import org.openjdk.jmh.annotations._
+import java.util.concurrent.TimeUnit
+import net.liftweb.json._
+
+case class Person(firstName:String, lastName:String, address:String, age:Int)
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class LiftJsonBenchmark  {
+  val p = Person("firstName", "lastName", "a long address with a carriage return\n ", 21)
+ 
+  implicit val df = DefaultFormats
+ 
+  @Benchmark
+  def serializeCaseClass = Serialization.write(p)
+}
